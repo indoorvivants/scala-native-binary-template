f86faf34ebd0614848c5b0684d7c8fdf6381887d
Anton Sviridov <keynmol@gmail.com>
2025-05-24 10:03:45 +0100

    Fix

--- project/ArtifactNaming.scala
+++ project/ArtifactNaming.scala
@@ -1,33 +1,42 @@
 import com.indoorvivants.detective.Platform
 import Platform.*
 
-object ArtifactNames:
-  def jarString(os: Platform.OS): String =
+object ArtifactNames {
+  def jarString(os: Platform.OS): String = {
     import Platform.OS.*
-    os match
+    os match {
       case Windows => "windows"
       case MacOS   => "osx"
       case Linux   => "linux"
       case Unknown => "unknown"
+    }
+  }
 
-  def jarString(bits: Platform.Bits, arch: Platform.Arch): String =
-    (bits, arch) match
+  def jarString(bits: Platform.Bits, arch: Platform.Arch): String = {
+    (bits, arch) match {
       case (Bits.x64, Arch.Intel) => "x86_64"
       case (Bits.x64, Arch.Arm)   => "aarch64"
       case (Bits.x32, Arch.Intel) => "x86_32"
       case (Bits.x32, Arch.Arm)   => "aarch32"
+    }
+  }
 
-  def jarString(target: Platform.Target): String =
+  def jarString(target: Platform.Target): String = {
     jarString(target.bits, target.arch) + "-" + jarString(target.os)
+  }
 
-  def coursierString(os: Platform.OS): String =
+  def coursierString(os: Platform.OS): String = {
     import Platform.OS.*
-    os match
+    os match {
       case Windows => "pc-win32"
       case MacOS   => "apple-darwin"
       case Linux   => "pc-linux"
       case Unknown => "unknown"
+    }
+  }
 
-  def coursierString(target: Platform.Target): String =
+  def coursierString(target: Platform.Target): String = {
     jarString(target.bits, target.arch) + "-" + coursierString(target.os)
-end ArtifactNames
+  }
+
+}
